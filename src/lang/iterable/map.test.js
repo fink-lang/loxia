6f38f72db2a2dc7af8bc72087b20e3584808f0f6
Jan Klaas Kollhof <jan@kollhof.net>
2020-03-28 18:41:01 +0100

    feat(map): allow spread as last expr to use map as flat_map

--- src/lang/iterable/map.test.js
+++ src/lang/iterable/map.test.js
@@ -1,19 +1,76 @@
 import {fink2js} from '../../testing';
 
 
-test('compiles map', ()=> {
-  expect(
-    fink2js(`
-      map item: item * 2
-      map item=123: item * 2
-      map {item}: item * 2
-      map [x, y]: x + y
+describe('map', ()=> {
+  it('compiles single line', ()=> {
+    expect(
+      fink2js(`
+        map item: item * 2
+      `)
+    ).toMatchSnapshot();
+  });
 
-      flat_map item: [spam + item]
 
-      flat_map item: [spam]
-    `)
-  ).toMatchSnapshot();
+  it('compiles multi line', ()=> {
+    expect(
+      fink2js(`
+        map item:
+          item * 2
+      `)
+    ).toMatchSnapshot();
+  });
+
+
+  it('compiles single line with default value', ()=> {
+    expect(
+      fink2js(`
+        map item=123: item * 2
+      `)
+    ).toMatchSnapshot();
+  });
+
+
+  it('compiles single line with destructured obj', ()=> {
+    expect(
+      fink2js(`
+        map {item}: item * 2
+      `)
+    ).toMatchSnapshot();
+  });
+
+
+  it('compiles single line with destructured array', ()=> {
+    expect(
+      fink2js(`
+        map [x, y]: x + y
+      `)
+    ).toMatchSnapshot();
+  });
+
+
+  it('compiles as flat map', ()=> {
+    expect(
+      fink2js(`
+        map [x, y]:
+          ...[x, y]
+      `)
+    ).toMatchSnapshot();
+  });
+});
+
+
+describe('flat_map', ()=> {
+
+  it('compiles', ()=> {
+    expect(
+      fink2js(`
+        flat_map item: [spam + item]
+
+        flat_map item: [spam]
+      `)
+    ).toMatchSnapshot();
+  });
+
 });
 
 
